9b6d9b778640e1280cbdadba854373fa57093945
Lonce Wyse <cmwll@nus.edu.sg>
2012-10-26 12:57:59 +0800

    clinet (helper app) now connects to both the network server (using 'net') and the local apps using UDP.

--- client.js
+++ client.js
@@ -1,62 +1,111 @@
 /*
 oscServer execution:
-node oscServer.js [clientHost] [clientPort] [remoteHost] [remotePort]
+node oscServer.js [clientHost] [from_clientPort] [to_clientPort] [remoteHost] [remotePort]
 Unless clientHost, clientPort, remoteHost and remotePort are specified, the
-program defaults to specified values
+program defaults to specified values.
+
+This program is a "helper" application that sets up a telnet connection with a server on the net,
+and passess messages between the server and a local client using UDP. The network server would 
+typically be a chatroom.
+
+The idea is to allow applications that know how to share UDP messages to do so over a WAN 
+rather than just a LAN. This is not unlike Ross Bencina's OSCGroups. 
 */
 
 var net = require('net');
 var dgram = require('dgram');
 
-var messageQueue = [];
-var CLIENT_HOST = process.argv[2] || "127.0.0.1";
-var CLIENT_PORT = process.argv[3] || 41234;
-var REMOTE_HOST = process.argv[4] || "animatedsoundworks.com";
-var REMOTE_PORT = process.argv[5] || 8001;
+var netMessageQueue = [];
+var CLIENT_HOST = process.argv[2] || "192.168.2.2";
+var FROM_CLIENT_PORT = process.argv[3] || 51080;
+var TO_CLIENT_PORT = process.argv[4] || 51180;
+var REMOTE_HOST = process.argv[5] || "animatedsoundworks.com";
+var REMOTE_PORT = process.argv[6] || 8001;
 
-var socket = net.connect(REMOTE_PORT, REMOTE_HOST);
-var client = dgram.createSocket("udp4");
+var netSocket = net.connect(REMOTE_PORT, REMOTE_HOST);
+var to_clientSocket = dgram.createSocket("udp4");
+var from_clientSocket = dgram.createSocket("udp4");
 
+var c2nCounter=0;
+var n2cCounter=0;
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// This section of the code listens for messages from the server on the net,
+// and passes the to a local port as UDP messages. 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // Periodically clears queue to a specified host and port
 (function (timeout, clientHost, clientPort) {
-	function clearQueue() {
+	function clearNetQueue() {
 		var buffMessage = "";
-		if (messageQueue.length > 0) {
-			buffMessage = messageQueue.shift();
-			client.send(buffMessage, 0, buffMessage.length, clientPort, clientHost, function (err, bytes) {
-				console.log("calling it again");
-				clearQueue();
+		if (netMessageQueue.length > 0) {
+			buffMessage = netMessageQueue.shift();
+			n2cCounter++;
+			to_clientSocket.send(buffMessage, 0, buffMessage.length, clientPort, clientHost, function (err, bytes) {
+				console.log(".......NET-2-CLIENT clearQ message #" + n2cCounter + "  being sent to port " + clientPort);
+				clearNetQueue();
 			});
 		} else {
-			setTimeout(clearQueue, timeout);
+			setTimeout(clearNetQueue, timeout);
 		}
 	}
-	clearQueue();
-}(100, CLIENT_HOST, CLIENT_PORT));
+	clearNetQueue();
+}(10, CLIENT_HOST, TO_CLIENT_PORT));
 
 // Add a connect listener
-socket.on('connect', function () {
-	console.log('Client has connected to the server!');
+netSocket.on('connect', function () {
+	console.log('HelperApp has connected to the net server!');
 });
 
 // Add a data listener
-socket.on('data', function (data) {
+netSocket.on('data', function (data) {
 	var i, charArray = [];
-	messageQueue.push(data);
-	console.log('Received a message from the server!', data);
+	netMessageQueue.push(data);
+	/*
 	data.forEach(function (byte) {
 		charArray.push(String.fromCharCode(byte));
 	});
-	console.log(charArray.join(""));
+*/
+	// console.log(charArray.join(""));
+
+	//console.log("data: " + data);
 });
 
 // Add a disconnect listener
-socket.on('disconnect', function () {
-	// Shouldn't this be "SERVER has disconnected"?
-	console.log('The client has disconnected!');
+netSocket.on('end', function () {
+	console.log('The set server has disconnected!');
+	process.exit(0);
 });
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Now for listening to the local client UDP messages and sending them to the server on the net.
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 // Sends a message to the server via sockets
 function sendMessageToServer(message) {
-	socket.send(message);
+	netSocket.write(message);
 }
+
+
+from_clientSocket.on("message", function (msg, rinfo) {
+	//console.log("from_client message from " + rinfo.address + ":" + rinfo.port);
+	//console.log("   << CLIENT-2-NET message: " + msg);
+	c2nCounter++;
+	console.log("   << CLIENT-2-NET message: " + c2nCounter);
+	sendMessageToServer("--" + msg);
+});
+
+/* no such thing as a 'data' event on dgram sockets
+from_clientSocket.on("data", function (data) {
+	sendMessageToServer("msg");
+	console.log("from client got data: " + data);
+});
+*/
+
+from_clientSocket.on("listening", function () {
+	var address = from_clientSocket.address();
+	console.log("listening " + address.address + ":" + address.port);
+});
+
+
+
+from_clientSocket.bind(FROM_CLIENT_PORT);
--- server.js
+++ server.js
@@ -1,40 +1,82 @@
-var net = require('net');
+net = require('net');
 
+var LISTEN_PORT = 8001;
 var sockets = [];
+var netMessageQueue = [];
 
-var s = net.Server(function (socket) {
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Periodically clears queue to a specified host and port
+
+(function (timeout) {
+    function clearNetQueue() {
+	var buffMessage = "";
+	var sock;
+	if (netMessageQueue.length > 0) {
+	    console.log("clearing messagequeue");
+//	    sock = netMessageQueue.shift();
+	    buffMessage = netMessageQueue.shift();
+	    sendtoclients(buffMessage, sock);
+	    clearNetQueue();
+	} else {
+	    setTimeout(clearNetQueue, timeout);
+	}
+    }
+    clearNetQueue(timeout);
+}(1000));
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+var s = net.Server(function(socket){
     sockets.push(socket);
-    socket.write('hello\n');
-    socket.write('world\n');
 
     console.log("Yo ---------------------  : " + socket.toString());
 
-    socket.on('data', function (d) {
-        var i;
-        for (i = 0; i < sockets.length; i += 1) {
-            if (sockets[i] !== socket) {
-                sockets[i].write(d);
-            }
-        }
-        console.log("received " + d);
+    socket.on('data', function(d){
+//	netMessageQueue.push(socket);
+	netMessageQueue.push(d);
+	console.log("received message from client" + d);
     });
 
-    socket.on('end', function () {
-        var i = sockets.indexOf(socket);
-        sockets.splice(i, 1);
+    socket.on('end', function(){
+	var i = sockets.indexOf(socket);
+	sockets.splice(i,1);
         console.log("console: dropping client " + i);
     });
 });
 
-s.listen(8001);
+s.listen(LISTEN_PORT);
+
+// sends to all sockets on list *except* sock (from whence it came)
+sendtoclients = function(d, isock){
+
+    var cleanup = []; // some sockets die without telling us about it. 
+    for(var i = 0; i < sockets.length; i++){
+	if (sockets[i] == isock) {
+	    return;
+	}
+	if (sockets[i].writable){
+	    sockets[i].write(d);
+	} else {
+	    cleanup.push(i); 
+	    sockets[i].destroy();
+	    console.log("will have to do clean up operation on an impolite socket");
+	}
+	console.log("sent to client  " + i + " the data " + d);
+    }
+    for(i=0;i<cleanup.length;i++){
+	sockets.splice(i,1);
+        console.log("console: dropping client " + i);
+    }
+}
 
 var t = 0;
-var tfunc = function () {
-    var i;
-    t += 5000;
-    for (i = 0; i < sockets.length; i += 1) {
-        sockets[i].write("t = " + t + "\n");
-    }
-};
+tfunc = function (){
+    t+=5000;
+    console.log("tick " + t);
+    sendtoclients("time " + t, null);
+}
+   
+//setInterval(tfunc, 5000);
 
-setInterval(tfunc, 5000);
